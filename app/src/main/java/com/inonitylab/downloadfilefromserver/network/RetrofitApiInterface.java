fa41ad251bc01a4357e97bce9d221b83f04aa23b
ruhul-inonity <ruhul.azam1@gmail.com>
2018-04-08 01:27:03 +0600

    service class and model added

---
+++ app/src/main/java/com/inonitylab/downloadfilefromserver/network/RetrofitApiInterface.java
@@ -0,0 +1,8 @@
+package com.inonitylab.downloadfilefromserver.network;
+
+/**
+ * Created by ruhul on 4/8/18.
+ */
+
+public interface RetrofitApiInterface {
+}
--- app/src/main/java/com/inonitylab/downloadfilefromserver/service/DownloaderService.java
+++ app/src/main/java/com/inonitylab/downloadfilefromserver/service/DownloaderService.java
@@ -1,8 +1,161 @@
 package com.inonitylab.downloadfilefromserver.service;
 
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Toast;
+
+import com.inonitylab.downloadfilefromserver.ui.MainActivity;
+import com.inonitylab.downloadfilefromserver.R;
+import com.inonitylab.downloadfilefromserver.model.Download;
+import com.inonitylab.downloadfilefromserver.network.RetrofitApiInterface;
+import com.inonitylab.downloadfilefromserver.util.Constants;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
 /**
  * Created by ruhul on 4/8/18.
  */
 
-public class DownloaderService {
+public class DownloaderService extends IntentService {
+
+    private static String FILE_EXTENSION = "videoFile.mp4";
+
+    public DownloaderService() {
+        super("Downloader Service");
+    }
+
+    private NotificationCompat.Builder notificationBuilder;
+    private NotificationManager notificationManager;
+    private int totalFileSize;
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Download")
+                .setContentText("Downloading File..")
+                .setAutoCancel(true);
+        notificationManager.notify(0, notificationBuilder.build());
+
+        initDownload();
+
+    }
+
+    /**
+     * creates retrofit instance using base url
+     */
+    private void initDownload() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .build();
+
+        RetrofitApiInterface retrofitInterface = retrofit.create(RetrofitApiInterface.class);
+
+        Call<ResponseBody> request = retrofitInterface.downloadFile();
+        try {
+
+            downloadFile(request.execute().body());
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void downloadFile(ResponseBody body) throws IOException {
+
+        int count;
+        byte data[] = new byte[1024 * 4];
+        long fileSize = body.contentLength();
+        InputStream bis = new BufferedInputStream(body.byteStream(), 1024 * 8);
+        File outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), FILE_EXTENSION);
+        OutputStream output = new FileOutputStream(outputFile);
+        long total = 0;
+        long startTime = System.currentTimeMillis();
+        int timeCount = 1;
+        while ((count = bis.read(data)) != -1) {
+
+            total += count;
+            totalFileSize = (int) (fileSize / (Math.pow(1024, 2)));
+            double current = Math.round(total / (Math.pow(1024, 2)));
+
+            int progress = (int) ((total * 100) / fileSize);
+
+            long currentTime = System.currentTimeMillis() - startTime;
+
+            Download download = new Download();
+            download.setTotalFileSize(totalFileSize);
+
+            if (currentTime > 1000 * timeCount) {
+
+                download.setCurrentFileSize((int) current);
+                download.setProgress(progress);
+                sendNotification(download);
+                timeCount++;
+            }
+
+            output.write(data, 0, count);
+        }
+        onDownloadComplete();
+        output.flush();
+        output.close();
+        bis.close();
+
+    }
+
+    private void sendNotification(Download download) {
+
+        sendIntent(download);
+        notificationBuilder.setProgress(100, download.getProgress(), false);
+        notificationBuilder.setContentText(String.format("Downloaded (%d/%d) MB", download.getCurrentFileSize(), download.getTotalFileSize()));
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+    private void sendIntent(Download download) {
+
+        Intent intent = new Intent(MainActivity.MESSAGE_PROGRESS);
+        intent.putExtra("download", download);
+        LocalBroadcastManager.getInstance(DownloaderService.this).sendBroadcast(intent);
+    }
+
+    private void onDownloadComplete() {
+
+        Download download = new Download();
+        download.setProgress(100);
+        sendIntent(download);
+
+        notificationManager.cancel(0);
+        notificationBuilder.setProgress(0, 0, false);
+        notificationBuilder.setContentText("File Successfully Downloaded.");
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        notificationManager.cancel(0);
+    }
+
 }
