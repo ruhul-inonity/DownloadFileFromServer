82d98ef28033fca6ec802044c15c9aa50a69a431
ruhul-inonity <ruhul.azam1@gmail.com>
2018-04-08 01:34:42 +0600

    ui added and file downloader implimented

--- app/src/main/java/com/inonitylab/downloadfilefromserver/ui/MainActivity.java
+++ app/src/main/java/com/inonitylab/downloadfilefromserver/ui/MainActivity.java
@@ -1,13 +1,154 @@
-package com.inonitylab.downloadfilefromserver;
+package com.inonitylab.downloadfilefromserver.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.inonitylab.downloadfilefromserver.R;
+import com.inonitylab.downloadfilefromserver.model.Download;
+import com.inonitylab.downloadfilefromserver.service.DownloaderService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE_PROGRESS = "message_progress";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    @BindView(R.id.progress)
+    ProgressBar mProgressBar;
+    @BindView(R.id.progress_text)
+    TextView mProgressText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        registerReceiver();
     }
+
+    /**
+     * Executes after clicking download button
+     * checks permission and fire download method
+     */
+    @OnClick(R.id.btn_download)
+    public void downloadFile(){
+
+        if(checkPermission()){
+            startDownload();
+        } else {
+            requestPermission();
+        }
+    }
+
+    /**
+     * starts downloader service
+     */
+    private void startDownload(){
+
+        Intent intent = new Intent(this,DownloaderService.class);
+        startService(intent);
+
+    }
+
+
+    /**
+     * registers broadcast manager and starts broadcasting in local channel
+     */
+    private void registerReceiver(){
+
+        LocalBroadcastManager bManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(MESSAGE_PROGRESS);
+        bManager.registerReceiver(broadcastReceiver, intentFilter);
+
+    }
+
+    /**
+     * retrieve progress and update ui
+     */
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(intent.getAction().equals(MESSAGE_PROGRESS)){
+
+                Download download = intent.getParcelableExtra("download");
+                mProgressBar.setProgress(download.getProgress());
+                if(download.getProgress() == 100){
+
+                    mProgressText.setText("File Download Complete");
+
+                } else {
+                    mProgressText.setText(String.format("Downloaded (%d/%d) MB",download.getCurrentFileSize(),download.getTotalFileSize()));
+                }
+            }
+        }
+    };
+
+    /**
+     * checks required permissions
+     * @return bool
+     */
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+
+    /**
+     * requests for permission
+     */
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
+    }
+
+
+    /**
+     * take action based on users permission requests response
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    startDownload();
+                } else {
+
+                    Snackbar.make(findViewById(R.id.coordinatorLayout),"Permission Denied, Please allow to proceed !", Snackbar.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
 }
